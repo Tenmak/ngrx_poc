b3272548ab299c3f2360c64c8f76e4b588b42166
BEUGNET Alex <alex.beugnet@akka.eu>
2018-05-23 15:50:05 +0200

    part 2 : add actions constant keys

--- src/app/people/people.ts
+++ src/app/people/people.ts
@@ -1,3 +1,11 @@
+import {
+  ADD_PERSON,
+  REMOVE_PERSON,
+  ADD_GUEST,
+  REMOVE_GUEST,
+  TOGGLE_ATTENDING
+} from '../actions/actions';
+
 /**
  * Person reducer
  * @param state Slice of whole people from store
@@ -5,7 +13,7 @@
  */
 export const peopleReducer = (state: People[] = [], action: any) => {
   switch (action.type) {
-    case 'ADD_PERSON':
+    case ADD_PERSON:
       const basePerson: Partial<People> = {
         id: action.payload.id,
         name: action.payload.name
@@ -14,15 +22,15 @@ export const peopleReducer = (state: People[] = [], action: any) => {
         ...state,
         Object.assign({}, createNewPerson(basePerson))
       ];
-    case 'REMOVE_PERSON':
+    case REMOVE_PERSON:
       console.log(action.payload);
       return state.filter(person => person.id !== action.payload);
     // to shorten our case statements, delegate detail updates to second private reducer
-    case 'ADD_GUEST':
+    case ADD_GUEST:
       return state.map(person => detailPersonReducer(person, action));
-    case 'REMOVE_GUEST':
+    case REMOVE_GUEST:
       return state.map(person => detailPersonReducer(person, action));
-    case 'TOGGLE_ATTENDING':
+    case TOGGLE_ATTENDING:
       return state.map(person => detailPersonReducer(person, action));
     // always have default return of previous state when action is not relevant
     default:
@@ -37,17 +45,17 @@ export const peopleReducer = (state: People[] = [], action: any) => {
  */
 const detailPersonReducer = (state: Partial<People> = {}, action: any) => {
   switch (action.type) {
-    case 'ADD_GUEST':
+    case ADD_GUEST:
       if (state.id === action.payload) {
         return Object.assign({}, state, { guests: state.guests + 1 } as Partial<People>);
       }
       return state;
-    case 'REMOVE_GUEST':
+    case REMOVE_GUEST:
       if (state.id === action.payload) {
         return Object.assign({}, state, { guests: state.guests - 1 } as Partial<People>);
       }
       return state;
-    case 'TOGGLE_ATTENDING':
+    case TOGGLE_ATTENDING:
       if (state.id === action.payload) {
         return Object.assign({}, state, { attending: !state.attending } as Partial<People>);
       }
