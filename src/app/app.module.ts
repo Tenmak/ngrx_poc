d7d2cea3f4ecb1ae1eb431ba645fe47de68c2669
BEUGNET Alex <alex.beugnet@akka.eu>
2018-05-23 10:45:25 +0200

    init ngrx dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
+import { reducers, metaReducers } from './reducers';
+import { Effects } from './.effects';
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([Effects])
   ],
   providers: [],
   bootstrap: [AppComponent]
